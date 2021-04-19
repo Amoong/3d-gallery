6c0d937fd14339799f2db012282bf0566f93f99c
amoong <moo1323@naver.com>
2021-04-19 20:35:27 +0900

    Init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Gallery from "components/Gallery";
+import { Reset } from "styled-reset";
+
+function App() {
+  return (
+    <>
+      <Reset />
+      <Gallery />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Gallery.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const GalleryContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: #222;
+`;
+
+function Gallery() {
+  return <GalleryContainer></GalleryContainer>;
+}
+
+export default Gallery;
