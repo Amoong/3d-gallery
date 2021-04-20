90f7831d3d17feadb8ac5cb0636be469681e642d
amoong <moo1323@naver.com>
2021-04-20 21:07:48 +0900

    Add Input form

--- src/routes/Auth.tsx
+++ src/routes/Auth.tsx
@@ -3,13 +3,49 @@ import { authService } from "fbase";
 import styled from "styled-components";
 
 import iconTypes from "constants/iconTypes";
+import React, { useState } from "react";
 
 const AuthContainer = styled.div``;
 
 function Auth() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
+  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
+    const { value, name } = e.target as HTMLInputElement;
+
+    switch (name) {
+      case "email":
+        setEmail(value);
+        break;
+      case "password":
+        setPassword(value);
+        break;
+    }
+  };
+
   return (
     <AuthContainer>
       <NavIcon iconType={iconTypes.HOME} to="/" />
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="text"
+          inputMode="email"
+          autoComplete="email"
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          name="password"
+          type="password"
+          value={password}
+          onChange={onChange}
+        />
+      </form>
     </AuthContainer>
   );
 }
